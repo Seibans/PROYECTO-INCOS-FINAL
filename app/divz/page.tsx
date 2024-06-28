1d27117094d69a6593447bac954a2dcd256686f8
Seibans <107325310+Seibans@users.noreply.github.com>
2024-06-28 13:11:32 -0400

    TODO ANTES DE 2FA

--- app/divz/page.tsx
+++ app/divz/page.tsx
@@ -1,54 +1,62 @@
 "use client";
 import { Divz } from "divz";
 import { BackButton } from "@/components/auth/back-button.component";
-import Image from "next/image";
+import Image from "next/image"
+
+import { AspectRatio } from "@/components/ui/aspect-ratio"
 
 
 const DivzPage = () => {
 	return (
 		<>
 			<Divz
-				showNavButtons={false}
-				isExpanded={true}>
+				showNavButtons={true}
+				isExpanded={false}>
 				<div>
-					1
-					<Image
-						src="https://res.cloudinary.com/dwakb0mcp/image/upload/v1698781153/productos/tfwkrzysg7fongtqhymt.png"
-						height="500"
-						width="500"
-						className="w-[80px] object-cover rounded-xl group-hover/card:shadow-xl"
-						alt="thumbnail"
-					/>
+					<AspectRatio ratio={2 / 3} className="bg-muted">
+					{/* 1 */}
+						<Image
+							src="https://res.cloudinary.com/dwakb0mcp/image/upload/v1698781153/productos/tfwkrzysg7fongtqhymt.png"
+							alt="Imagen"
+							className="rounded-md object-cover"
+							fill
+							// sizes="(max-width: 768px) 1vw, (max-width: 1280px) 50vw, (max-width: 1920px) 25vw"
+						/>
+					</AspectRatio>
 				</div>
 				<div>
-					2
-					<Image
-						src="https://res.cloudinary.com/dwakb0mcp/image/upload/v1698781102/productos/md0mpornuckk5ozucpo3.png"
-						height="500"
-						width="500"
-						className="w-[80px] object-cover rounded-xl group-hover/card:shadow-xl"
-						alt="thumbnail"
-					/>	
+					{/* 2 */}
+					<AspectRatio ratio={2 / 3} className="bg-muted">
+						<Image
+							src="https://res.cloudinary.com/dwakb0mcp/image/upload/v1698781102/productos/md0mpornuckk5ozucpo3.png"
+							className="object-cover rounded-xl group-hover/card:shadow-xl"
+							alt="thumbnail"
+							fill
+						/>
+					</AspectRatio>
+
 				</div>
 				<div>
-					3
-					<Image
-						src="https://res.cloudinary.com/dwakb0mcp/image/upload/v1698781264/productos/t8g1dotgiz9mc68nttrd.png"
-						height="500"
-						width="500"
-						className="w-[80px] object-cover rounded-xl group-hover/card:shadow-xl"
-						alt="thumbnail"
-					/>
+					{/* 3 */}
+					<AspectRatio ratio={2 / 3} className="bg-muted">
+						<Image
+							src="https://res.cloudinary.com/dwakb0mcp/image/upload/v1698781264/productos/t8g1dotgiz9mc68nttrd.png"
+							className="object-cover rounded-xl group-hover/card:shadow-xl"
+							alt="thumbnail"
+							fill
+						/>
+					</AspectRatio>
 				</div>
 				<div>
-					4
-					<Image
-						src="https://res.cloudinary.com/dwakb0mcp/image/upload/v1698781362/productos/u2aodv72r41cmishkxnw.png"
-						height="500"
-						width="500"
-						className="w-[80px] object-cover rounded-xl group-hover/card:shadow-xl"
-						alt="thumbnail"
-					/>
+					{/* 4 */}
+					<AspectRatio ratio={2 / 3} className="bg-muted">
+						<Image
+							src="https://res.cloudinary.com/dwakb0mcp/image/upload/v1698781362/productos/u2aodv72r41cmishkxnw.png"
+							className="object-cover rounded-xl group-hover/card:shadow-xl"
+							alt="thumbnail"
+							fill
+						/>
+					</AspectRatio>
 				</div>
 			</Divz>
 			<div className="absolute left-1/2 transform -translate-x-1/2 bottom-4 z-10">
